4777090b343a1b65c56741758984ce26f64724c4
SIHAB360 <brothersfreelancing8@gmail.com>
2026-09-06 03:18:10 +0600

    Update Navbar.jsx

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,37 +1,22 @@
-import Image from "next/image";
-
 export default function Navbar() {
   return (
     <nav className="navbar">
 
-      <div className="logo-area">
-
-        <Image
-  src="/logo.png"
-  width={45}
-  height={45}
-  alt="OP Logo"
-/>
-
-        <h2>
-          OVER POWER <span>ESPORTS</span>
-        </h2>
-
+      <div className="logo">
+        OVER POWER <span>ESPORTS</span>
       </div>
 
 
       <div className="nav-links">
-
         <a href="#">Home</a>
         <a href="#">Players</a>
         <a href="#">Teams</a>
         <a href="#">Tournaments</a>
         <a href="#">Rankings</a>
-
       </div>
 
 
-      <button className="join-btn">
+      <button className="nav-btn">
         Join Team
       </button>
 
