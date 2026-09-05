c41693f2065c68715a56c11f09673a4bd127d076
SIHAB360 <brothersfreelancing8@gmail.com>
2026-09-06 02:38:16 +0600

    Update Navbar.jsx

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,20 +1,36 @@
+import Image from "next/image";
+
 export default function Navbar() {
   return (
     <nav className="navbar">
 
-      <div className="logo">
-        OVER POWER <span>ESPORTS</span>
+      <div className="logo-area">
+
+        <Image
+          src="/logo.png"
+          width={45}
+          height={45}
+          alt="Over Power Esports Logo"
+          className="logo"
+        />
+
+        <h2>
+          OVER POWER <span>ESPORTS</span>
+        </h2>
+
       </div>
+
 
       <div className="nav-links">
-        <a href="#">Home</a>
-        <a href="#">Players</a>
-        <a href="#">Teams</a>
-        <a href="#">Tournaments</a>
-        <a href="#">Rankings</a>
+        <a>Home</a>
+        <a>Players</a>
+        <a>Teams</a>
+        <a>Tournaments</a>
+        <a>Rankings</a>
       </div>
 
-      <button className="nav-btn">
+
+      <button>
         Join Team
       </button>
 
