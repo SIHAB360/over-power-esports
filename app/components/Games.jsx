1db4eaa2dcfc3df7ce328185b2e1f43c27792e8a
SIHAB360 <brothersfreelancing8@gmail.com>
2026-09-06 21:40:55 +0600

    Create Games.jsx

---
+++ app/components/Games.jsx
@@ -0,0 +1,47 @@
+export default function Games(){
+
+return(
+
+<section className="games">
+
+<h2>
+Games We Dominate
+</h2>
+
+
+<div className="game-card">
+
+
+<h3>
+🔥 Free Fire
+</h3>
+
+
+<p>
+Free Fire Competitive Team
+</p>
+
+
+<p>
+🏆 Tournaments: 25+
+</p>
+
+
+<p>
+🥇 Wins: 15+
+</p>
+
+
+<span>
+Active Division
+</span>
+
+
+</div>
+
+
+</section>
+
+)
+
+}
