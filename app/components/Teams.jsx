e4626dfd5b4ec33c44376d616f75c793a0ebce9c
SIHAB360 <brothersfreelancing8@gmail.com>
2026-09-09 01:09:12 +0600

    Update Teams.jsx

--- app/components/Teams.jsx
+++ app/components/Teams.jsx
@@ -1,5 +1,15 @@
-export default function Teams(){
 import ProfileLoader from "./ProfileLoader";
+
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaYoutube,
+  FaTiktok
+} from "react-icons/fa";
+
+
+export default function Teams(){
+
 const teams = [
 
 {
@@ -15,9 +25,9 @@ image:"/players/appelo.png",
 position:"center top",
 social:{
 facebook:"https://www.facebook.com/share/19XKoR58cb/?mibextid=wwXIfr",
-instagram:"https://www.instagram.com/_appelo_ff?stkn=ZjBta2pzMmJodjU2&utm_source=qr",
-youtube:"https://youtube.com/@appelo_ff?si=xNMDlDdjx7jQ2Bqp",
-tiktok:"https://www.tiktok.com/@appelo_offical_?_r=1&_t=ZS-99XT4cbGFoj"
+instagram:"https://www.instagram.com/_appelo_ff",
+youtube:"https://youtube.com/@appelo_ff",
+tiktok:"https://www.tiktok.com/@appelo_offical_"
 }
 },
 
@@ -28,9 +38,9 @@ image:"/players/oggy.png",
 position:"center top",
 social:{
 facebook:"https://www.facebook.com/share/1JEkiAK2J6/",
-instagram:"https://www.instagram.com/being_ogggyy?stkn=MTRuemcwaDJjaWdvMg==",
-youtube:"https://youtube.com/@being_ogggyy?si=UHNwx-WcCr4kVwFz",
-tiktok:"https://www.tiktok.com/@being_ogggyy?_r=1&_t=ZS-99Z8hvW8Hxd"
+instagram:"https://www.instagram.com/being_ogggyy",
+youtube:"https://youtube.com/@being_ogggyy",
+tiktok:"https://www.tiktok.com/@being_ogggyy"
 }
 },
 
@@ -41,9 +51,8 @@ image:"/players/itachix.png",
 position:"center top",
 social:{
 facebook:"https://www.facebook.com/profile.php?id=61590102347309",
-instagram:"",
-youtube:"https://youtube.com/@itachiontop-r2p?si=59-V9LBT1yH7d-L5",
-tiktok:"https://www.tiktok.com/@itachix074?_r=1&_t=ZS-99XT9LGrTPM"
+youtube:"https://youtube.com/@itachiontop-r2p",
+tiktok:"https://www.tiktok.com/@itachix074"
 }
 },
 
@@ -53,10 +62,10 @@ role:"IGL+SUPPORTER",
 image:"/players/rejwan.png",
 position:"center top",
 social:{
-facebook:"https://www.facebook.com/rejwan.ahammed11?mibextid=wwXIfr&mibextid=wwXIfr",
-instagram:"https://www.instagram.com/rahammed_?stkn=MThpaDNkczhodzZiMQ%3D%3D&utm_source=qr",
-youtube:"https://youtube.com/@rejwan-ff6711?si=wkjwjecveyM69IRr",
-tiktok:"https://www.tiktok.com/@rejwanahammed?_r=1&_t=ZS-99Z8sfhqSbo"
+facebook:"https://www.facebook.com/rejwan.ahammed11",
+instagram:"https://www.instagram.com/rahammed_",
+youtube:"https://youtube.com/@rejwan-ff6711",
+tiktok:"https://www.tiktok.com/@rejwanahammed"
 }
 },
 
@@ -67,7 +76,7 @@ image:"/players/fixfire.png",
 position:"center top",
 social:{
 facebook:"https://www.facebook.com/share/1DYEWmWzRr/",
-instagram:"https://www.instagram.com/xr_jisan09?stkn=b3d6a2Y4MTZuMXg1",
+instagram:"https://www.instagram.com/xr_jisan09",
 youtube:"https://www.youtube.com/@fixfire09",
 tiktok:"https://tiktok.com/@fixfire09"
 }
@@ -75,99 +84,9 @@ tiktok:"https://tiktok.com/@fixfire09"
 
 ]
 
-},
-
-
-{
-name:"OVER POWER ELITE",
-slogan:"Victory Is Our Language",
-
-players:[
-
-{
-name:"FOYSAL",
-role:"PRIMARY",
-image:"/players/rfntc.png",
-position:"center top",
-social:{}
-},
-
-{
-name:"JELLAL",
-role:"SECONDARY",
-image:"/players/jellal.png",
-position:"center 15%",
-social:{}
-},
-
-{
-name:"SOJIB",
-role:"BOMBER",
-image:"/players/sojib.png",
-position:"center top",
-social:{}
-},
-
-{
-name:"NAFIZ",
-role:"SUPPORTER",
-image:"/players/nafiz.jpeg",
-position:"center 20%",
-social:{}
-},
-
-{
-name:"BAYMAX",
-role:"SNIPER",
-image:"/players/baymax.png",
-position:"center top",
-social:{}
 }
 
-]
-
-},
-
-
-{
-name:"OVER POWER RISING",
-slogan:"Future Champions",
-
-players:[
-{
-name:"PLAYER ONE",
-role:"RISING STAR",
-social:{}
-},
-
-{
-name:"PLAYER TWO",
-role:"ASSAULTER",
-social:{}
-},
-
-{
-name:"PLAYER THREE",
-role:"SNIPER",
-social:{}
-},
-
-{
-name:"PLAYER FOUR",
-role:"ALL ROUNDER",
-social:{}
-},
-
-{
-name:"PLAYER FIVE",
-role:"SUPPORT",
-social:{}
-}
-]
-
-}
-
-]
+];
 
 
 return(
@@ -196,6 +115,7 @@ teams.map((team,index)=>(
 </p>
 
 
+
 <div className="team-players">
 
 
@@ -208,15 +128,16 @@ team.players.map((player,i)=>(
 
 <div className="player-image">
 
-<img 
+<img
 src={player.image}
 alt={player.name}
 style={{
-objectPosition:player.position || "center top"
+objectPosition:player.position || "center"
 }}
 />
 
 </div>
+
 
 
 <h4>
@@ -234,56 +155,63 @@ objectPosition:player.position || "center top"
 
 
 {
-player.social?.facebook &&
+player.social.facebook &&
 
 <a
 href={player.social.facebook}
 target="_blank"
 rel="noopener noreferrer"
+className="facebook"
 >
-<i className="fa-brands fa-facebook-f"></i>
+<FaFacebookF/>
 </a>
 
 }
 
 
+
 {
-player.social?.instagram &&
+player.social.instagram &&
 
 <a
 href={player.social.instagram}
 target="_blank"
 rel="noopener noreferrer"
+className="instagram"
 >
-<i className="fa-brands fa-instagram"></i>
+<FaInstagram/>
 </a>
 
 }
 
 
+
 {
-player.social?.youtube &&
+player.social.youtube &&
 
 <a
 href={player.social.youtube}
 target="_blank"
 rel="noopener noreferrer"
+className="youtube"
 >
-<i className="fa-brands fa-youtube"></i>
+<FaYoutube/>
 </a>
 
 }
 
 
+
 {
-player.social?.tiktok &&
+player.social.tiktok &&
 
 <a
 href={player.social.tiktok}
 target="_blank"
 rel="noopener noreferrer"
+className="tiktok"
 >
-<i className="fa-brands fa-tiktok"></i>
+<FaTiktok/>
 </a>
 
 }
@@ -293,7 +221,7 @@ rel="noopener noreferrer"
 
 
 
-<ProfileLoader id={player.id}/>
+<ProfileLoader/>
 
 
 </div>
