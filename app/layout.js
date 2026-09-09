63b3010f69c47c0f683c8b6b92eadc4cffa8b606
SIHAB360 <brothersfreelancing8@gmail.com>
2026-09-09 21:59:16 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,29 +1,40 @@
 import "./globals.css";
 
+
 export const metadata = {
   title: "Over Power Esports",
   description: "Victory is our Mission",
 };
 
+
+
 export default function RootLayout({ children }) {
+
   return (
+
     <html lang="en">
+
 
       <head>
 
-       <link
-  rel="stylesheet"
-  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
-/>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        />
 
       </head>
 
 
+
       <body>
+
         {children}
+
       </body>
 
 
     </html>
+
   );
+
 }
