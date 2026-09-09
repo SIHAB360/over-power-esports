f7e856c7acc0e458dc5cbf16131a37ad2df27998
SIHAB360 <brothersfreelancing8@gmail.com>
2026-09-10 01:01:18 +0600

    Update FloatingContact.jsx

--- app/components/FloatingContact.jsx
+++ app/components/FloatingContact.jsx
@@ -23,12 +23,21 @@ export default function FloatingContact() {
 
 
   return (
+  <div
+    style={{
+      position: "fixed",
+      right: "30px",
+      bottom: "30px",
+      width: "70px",
+      height: "70px",
+      background: "red",
+      borderRadius: "50%",
+      zIndex: 999999
+    }}
+  >
 
-    <div
-      className={`floating-contact ${open ? "is-open" : ""}`}
-      onMouseEnter={() => setOpen(true)}
-      onMouseLeave={() => setOpen(false)}
-    >
+  </div>
+);
 
 
       <div className="floating-links">
