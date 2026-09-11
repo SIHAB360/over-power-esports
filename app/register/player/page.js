93c66e6905e56ba590dd125f11bae526760e5339
SIHAB360 <brothersfreelancing8@gmail.com>
2026-09-11 19:52:37 +0600

    Create page.js

---
+++ app/register/player/page.js
@@ -0,0 +1,17 @@
+export default function PlayerRegister(){
+
+  return (
+    <main>
+
+      <h1>
+        Player Registration
+      </h1>
+
+      <p>
+        Welcome to Over Power Esports
+      </p>
+
+    </main>
+  );
+
+}
