8b45157df7ed814c20201da0759df8e5434724d6
SIHAB360 <brothersfreelancing8@gmail.com>
2026-09-11 13:27:05 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,40 +1,86 @@
 import "./globals.css";
 
+import { Orbitron, Rajdhani } from "next/font/google";
+
+
+const orbitron = Orbitron({
+
+  subsets:["latin"],
+
+  weight:[
+    "400",
+    "500",
+    "700",
+    "800",
+    "900"
+  ],
+
+  variable:"--font-orbitron",
+
+});
+
+
+
+const rajdhani = Rajdhani({
+
+  subsets:["latin"],
+
+  weight:[
+    "400",
+    "500",
+    "600",
+    "700"
+  ],
+
+  variable:"--font-rajdhani",
+
+});
+
+
+
 
 export const metadata = {
+
   title: "Over Power Esports",
+
   description: "Victory is our Mission",
+
 };
+
 
 
 
 export default function RootLayout({ children }) {
 
-  return (
 
-    <html lang="en">
+return (
 
-
-      <head>
-
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
-        />
-
-      </head>
+<html lang="en">
 
 
+<head>
 
-      <body>
+<link
+rel="stylesheet"
+href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+/>
 
-        {children}
-
-      </body>
+</head>
 
 
-    </html>
 
-  );
+
+<body className={`${orbitron.variable} ${rajdhani.variable}`}>
+
+{children}
+
+</body>
+
+
+
+
+</html>
+
+);
 
 }
