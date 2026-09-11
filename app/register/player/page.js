e47e808214f75f384c6dbb88233a657362370eac
SIHAB360 <brothersfreelancing8@gmail.com>
2026-09-11 20:15:53 +0600

    Update page.js

--- app/register/player/page.js
+++ app/register/player/page.js
@@ -4,41 +4,127 @@ import { useState } from "react";
 
 export default function PlayerRegister(){
 
-  const [name,setName]=useState("");
-  const [team,setTeam]=useState("");
+  const [form,setForm] = useState({
+    full_name:"",
+    ign:"",
+    freefire_uid:"",
+    email:"",
+    phone:"",
+    age:"",
+    team_name:""
+  });
 
-  function submit(e){
+
+  const [message,setMessage]=useState("");
+
+
+  function handleChange(e){
+
+    setForm({
+      ...form,
+      [e.target.name]:e.target.value
+    });
+
+  }
+
+
+
+  async function submit(e){
+
     e.preventDefault();
 
-    console.log({
-      name,
-      team
+
+    const res = await fetch("/api/register-player",{
+
+      method:"POST",
+
+      headers:{
+        "Content-Type":"application/json"
+      },
+
+      body:JSON.stringify(form)
+
     });
+
+
+    const data = await res.json();
+
+
+    if(data.success){
+
+      setMessage("Registration Successful");
+
+    }
+    else{
+
+      setMessage(data.message);
+
+    }
+
   }
+
 
 
   return (
 
     <main>
 
+
       <h1>
         PLAYER REGISTRATION
       </h1>
 
 
+
       <form onSubmit={submit}>
 
+
         <input
-          placeholder="Player Name"
-          value={name}
-          onChange={(e)=>setName(e.target.value)}
+        name="full_name"
+        placeholder="Player Name"
+        onChange={handleChange}
         />
 
 
         <input
-          placeholder="Team Name"
-          value={team}
-          onChange={(e)=>setTeam(e.target.value)}
+        name="ign"
+        placeholder="In Game Name"
+        onChange={handleChange}
+        />
+
+
+        <input
+        name="freefire_uid"
+        placeholder="Free Fire UID"
+        onChange={handleChange}
+        />
+
+
+        <input
+        name="email"
+        placeholder="Email"
+        onChange={handleChange}
+        />
+
+
+        <input
+        name="phone"
+        placeholder="Phone Number"
+        onChange={handleChange}
+        />
+
+
+        <input
+        name="age"
+        placeholder="Age"
+        onChange={handleChange}
+        />
+
+
+        <input
+        name="team_name"
+        placeholder="Team Name"
+        onChange={handleChange}
         />
 
 
@@ -46,7 +132,11 @@ export default function PlayerRegister(){
           Submit
         </button>
 
+
       </form>
+
+
+      <p>{message}</p>
 
 
     </main>
