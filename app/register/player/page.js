7834e0ab65392c13bae4f6d1fd9c5f588a4b1e8d
SIHAB360 <brothersfreelancing8@gmail.com>
2026-09-11 19:53:49 +0600

    Update page.js

--- app/register/player/page.js
+++ app/register/player/page.js
@@ -1,17 +1,56 @@
+"use client";
+
+import { useState } from "react";
+
 export default function PlayerRegister(){
 
+  const [name,setName]=useState("");
+  const [team,setTeam]=useState("");
+
+  function submit(e){
+    e.preventDefault();
+
+    console.log({
+      name,
+      team
+    });
+  }
+
+
   return (
+
     <main>
 
       <h1>
-        Player Registration
+        PLAYER REGISTRATION
       </h1>
 
-      <p>
-        Welcome to Over Power Esports
-      </p>
+
+      <form onSubmit={submit}>
+
+        <input
+          placeholder="Player Name"
+          value={name}
+          onChange={(e)=>setName(e.target.value)}
+        />
+
+
+        <input
+          placeholder="Team Name"
+          value={team}
+          onChange={(e)=>setTeam(e.target.value)}
+        />
+
+
+        <button>
+          Submit
+        </button>
+
+      </form>
+
 
     </main>
+
   );
 
 }
