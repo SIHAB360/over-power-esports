f267aec6c5663e85aa517aa96db02c5eb29e19b3
SIHAB360 <brothersfreelancing8@gmail.com>
2026-09-14 23:43:39 +0600

    Update page.js

--- app/login/page.js
+++ app/login/page.js
@@ -1,1 +1,43 @@
 
+"use client";
+
+import { useState } from "react";
+
+export default function LoginPage() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+
+  return (
+    <main>
+
+      <h1>
+        Login
+      </h1>
+
+
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e)=>setEmail(e.target.value)}
+      />
+
+
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e)=>setPassword(e.target.value)}
+      />
+
+
+      <button>
+        Login
+      </button>
+
+
+    </main>
+  );
+}
