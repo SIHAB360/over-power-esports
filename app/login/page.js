35ce825b14c894e7cd881ee92924b4f9f241c2a5
SIHAB360 <brothersfreelancing8@gmail.com>
2026-09-14 23:59:24 +0600

    Update page.js

--- app/login/page.js
+++ app/login/page.js
@@ -12,76 +12,146 @@ export default function LoginPage() {
 
 
 
- const handleLogin = async () => {
-
-  alert("Button clicked");
+  const handleLogin = async () => {
 
 
-  const { data, error } = await supabase.auth.signInWithPassword({
-
-    email,
-
-    password,
-
-  });
+    alert("Button clicked");
 
 
 
-  if(error){
+    const { data, error } = await supabase.auth.signInWithPassword({
 
-    alert(error.message);
+      email,
 
-    return;
+      password,
 
-  }
-
-
-
-  alert("Auth Success");
+    });
 
 
 
-  const { data: profile, error: profileError } = await supabase
-    .from("profiles")
-    .select("role")
-    .eq("id", data.user.id)
-    .single();
+    if(error){
+
+      alert(error.message);
+
+      return;
+
+    }
 
 
 
-  if(profileError){
-
-    alert(profileError.message);
-
-    return;
-
-  }
+    alert("Auth Success");
 
 
 
-  alert("Role: " + profile.role);
+    const { data: profile, error: profileError } = await supabase
+      .from("profiles")
+      .select("role")
+      .eq("id", data.user.id)
+      .single();
 
 
 
-  if(profile.role === "admin"){
+    if(profileError){
 
-    window.location.href="/admin/dashboard";
+      alert(profileError.message);
 
-  }
+      return;
 
-
-  else if(profile.role === "player"){
-
-    window.location.href="/player/dashboard";
-
-  }
+    }
 
 
-  else{
 
-    alert("Invalid Role");
-
-  }
+    alert("Role: " + profile.role);
 
 
-};
+
+    if(profile.role === "admin"){
+
+
+      window.location.href="/admin/dashboard";
+
+
+    }
+
+
+
+    else if(profile.role === "player"){
+
+
+      window.location.href="/player/dashboard";
+
+
+    }
+
+
+
+    else{
+
+
+      alert("Invalid Role");
+
+
+    }
+
+
+
+  };
+
+
+
+  return (
+
+    <main>
+
+
+      <h1>
+        Login
+      </h1>
+
+
+
+      <input
+
+        type="email"
+
+        placeholder="Email"
+
+        value={email}
+
+        onChange={(e)=>setEmail(e.target.value)}
+
+      />
+
+
+
+      <input
+
+        type="password"
+
+        placeholder="Password"
+
+        value={password}
+
+        onChange={(e)=>setPassword(e.target.value)}
+
+      />
+
+
+
+      <button
+
+        onClick={handleLogin}
+
+      >
+
+        Login
+
+      </button>
+
+
+
+    </main>
+
+  );
+
+}
