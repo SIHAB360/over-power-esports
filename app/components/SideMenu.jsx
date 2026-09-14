4f36322d6b776c205f6c0d33d85f713a1593b748
SIHAB360 <brothersfreelancing8@gmail.com>
2026-09-14 20:06:02 +0600

    Update SideMenu.jsx

--- app/components/SideMenu.jsx
+++ app/components/SideMenu.jsx
@@ -1,1 +1,16 @@
 
+"use client";
+
+export default function SideMenu({ open, close }) {
+  if (!open) return null;
+
+  return (
+    <div>
+      <h1>Side Menu</h1>
+
+      <button onClick={close}>
+        Close
+      </button>
+    </div>
+  );
+}
