6db5bf74eda1eec37d10c63f06205ee5aae130d7
SIHAB360 <brothersfreelancing8@gmail.com>
2026-09-17 02:05:49 +0600

    Update supabase.js

--- app/lib/supabase.js
+++ app/lib/supabase.js
@@ -7,6 +7,7 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 
+
 export const supabase = createClient(
   supabaseUrl,
   supabaseAnonKey
