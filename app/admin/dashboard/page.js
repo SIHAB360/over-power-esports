2c72d3a21bbb1189032560009aad789a9e36afdc
SIHAB360 <brothersfreelancing8@gmail.com>
2026-09-17 02:33:28 +0600

    Update page.js

--- app/admin/dashboard/page.js
+++ app/admin/dashboard/page.js
@@ -1,6 +1,7 @@
 "use client";
 import { supabase } from "../../lib/supabase";
 import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function AdminDashboard() {
   const [loading, setLoading] = useState(true);
