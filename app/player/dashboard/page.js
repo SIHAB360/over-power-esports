420670aa7ae1580fb5dba13e7611cb232d089b1b
SIHAB360 <brothersfreelancing8@gmail.com>
2026-09-18 15:24:45 +0600

    Update page.js

--- app/player/dashboard/page.js
+++ app/player/dashboard/page.js
@@ -1,6 +1,44 @@
 "use client";
 
+import { useEffect } from "react";
+import { supabase } from "../../lib/supabase";
+import { useRouter } from "next/navigation";
+
 export default function PlayerDashboard() {
+  const router = useRouter();
+
+useEffect(() => {
+
+  checkUser();
+
+}, []);
+
+
+const checkUser = async () => {
+
+  const {
+    data: { user }
+  } = await supabase.auth.getUser();
+
+
+  if (!user) {
+    router.push("/login");
+    return;
+  }
+
+
+  const { data: profile } = await supabase
+    .from("profiles")
+    .select("role")
+    .eq("id", user.id)
+    .single();
+
+
+  if (profile?.role !== "player") {
+    router.push("/");
+  }
+
+};
   return (
     <main
       style={{
