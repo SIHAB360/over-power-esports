6ec359b38902a83d729b9a7ae2baae13a3a76b0e
SIHAB360 <brothersfreelancing8@gmail.com>
2026-09-20 21:02:34 +0600

    Update page.js

--- app/admin/tournaments/[id]/page.js
+++ app/admin/tournaments/[id]/page.js
@@ -14,6 +14,8 @@ export default function TournamentDetails(){
 
   const [tournament,setTournament] = useState(null);
   const [loading,setLoading] = useState(true);
+    const [teams,setTeams] = useState([]);
+    const [showTeams,setShowTeams] = useState(false);
 
 
 
