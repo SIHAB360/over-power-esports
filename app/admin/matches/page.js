ee082d039ff8d40a7b8ba0b1c684f6394f01e51f
SIHAB360 <brothersfreelancing8@gmail.com>
2026-09-21 21:29:23 +0600

    Update page.js

--- app/admin/matches/page.js
+++ app/admin/matches/page.js
@@ -787,12 +787,12 @@ export default function AdminMatchesPage() {
 
 
       setMessage(
-        form.status === "completed"
-          ? `Match saved. Each active player receives ৳${money(
-              perPlayerAmount
-            )}.`
-          : "Match created successfully."
-      );
+  form.status === "completed"
+    ? `Match saved. Each active player receives ${money(
+        perPlayerAmount
+      )}`
+    : "Match created successfully."
+);
 
       setMessageType("success");
 
