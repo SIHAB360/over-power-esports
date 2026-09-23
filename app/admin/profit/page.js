3f6447516f2b8422e463e0f1c2228925f3e8c13f
SIHAB360 <brothersfreelancing8@gmail.com>
2026-09-24 01:51:33 +0600

    Update page.js

--- app/admin/profit/page.js
+++ app/admin/profit/page.js
@@ -23,30 +23,24 @@ export default function ProfitPage() {
 const { data, error } = await supabase
 .from("match_finance")
 .select(`
- *,
- matches!match_finance_match_id_fkey (
-   id,
-   tournament_id,
-   match_type,
-   created_at
- ),
- teams!match_finance_team_id_fkey (
-   id,
-   team_name
- )
+  *,
+  matches!match_finance_match_id_fkey (
+    id,
+    tournament_id,
+    match_type,
+    created_at,
+    tournaments (
+      name
+    )
+  ),
+  teams!match_finance_team_id_fkey (
+    id,
+    team_name
+  )
 `)
 .order("created_at", {
- ascending:false
+  ascending:false
 });
-
-
-    if(error){
-
-      console.log(
-        "PROFIT FETCH ERROR:",
-        JSON.stringify(error,null,2)
-      );
-
 
       setLoading(false);
 
