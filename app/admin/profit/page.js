13b1a5010143b1395c0f9a231a5b05d1f3350f86
SIHAB360 <brothersfreelancing8@gmail.com>
2026-09-24 22:58:20 +0600

    Update page.js

--- app/admin/profit/page.js
+++ app/admin/profit/page.js
@@ -14,6 +14,7 @@ export default function ProfitPage() {
     fetchFinance();
   }, []);
 
+  
   const fetchFinance = async () => {
     const { data, error } = await supabase
       .from("match_finance")
